ae8157a619eb968ab795cadebfdbb7b16d856edd
jthulhu <adrien.lc.mathieu@gmail.com>
2023-05-18 16:35:47 +0200

    [examples] Improved an example.

--- examples/poly/fonction1.c
+++ examples/poly/fonction1.c
@@ -5,5 +5,13 @@ int add(int x, int y) {
 void main() {
   int x = rand(0, 10);
   if (add(x, 3) == 10)
-    assert(x == 7); // @OK
+    assert(x == 7); //@OK
+  else
+    // Ça ne peut pas marcher parce que `x` pourrait appartenir à deux intervals disjoints.
+    assert(x != 7); //@KO
+
+  if (add(x, 3) == 13)
+    assert(x == 10); //@OK
+  else
+    assert(x < 10); //@OK
 }
